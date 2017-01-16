dd12df21c0c83e26a92d6413d2ce32241dee3e5b
Joe Reeve <joseph.reeve@googlemail.com>
2017-01-16 18:11:55 +0000

    Changes to how we copy files

--- steps/export.js
+++ steps/export.js
@@ -6,79 +6,41 @@ const fs = require('fs');
 const config = Object.assign({ languages: ['en'] }, require('../config.json'));
 const spawn = require('child_process').spawn;
 const dirsum = require('dirsum');
+const ncp = require('ncp');
+ncp.limit = 16;
 
-const templateHTML = fs.readFileSync(resDir + 'template.html', 'utf8');
+ncp(resDir + 'ractive.js', outDir + 'ractive.js');
+ncp(resDir + 'index.css', outDir + 'index.css');
+ncp(resDir + 'phet-banner.png', outDir + 'phet-banner.png');
+ncp(resDir + 'favicon.png', outDir + 'favicon.png');
 
-const copyFile = (fromPath, toPath) => {
-  return fs.createReadStream(fromPath).pipe(fs.createWriteStream(toPath));
-};
-
-copyFile(resDir + 'ractive.js', outDir + 'ractive.js');
-copyFile(resDir + 'index.css', outDir + 'index.css');
-copyFile(resDir + 'phet-banner.png', outDir + 'phet-banner.png');
-copyFile(resDir + 'favicon.png', outDir + 'favicon.png');
-
-
-copyFileWorker = (index, step, files) => { //TODO: Refactor using highland
-  const fileName = files[index];
-  if(!fileName) return;
-  copyFile(inDir + fileName, outDir + fileName).on('close', function(){
-    copyFileWorker(index + step, step, files);
-  });
-};
 
 const files = fs.readdirSync(inDir);
-
-//This only does 10 at a time, most machines can do better
-copyFileWorker(0, 10, files); //TODO: Refactor into a workerGenerator
-copyFileWorker(1, 10, files);
-copyFileWorker(2, 10, files);
-copyFileWorker(3, 10, files);
-copyFileWorker(4, 10, files);
-copyFileWorker(5, 10, files);
-copyFileWorker(6, 10, files);
-copyFileWorker(7, 10, files);
-copyFileWorker(8, 10, files);
-copyFileWorker(9, 10, files);
-
-fs.writeFileSync(outDir + 'index.html', //Pretty hacky - doing a replace on the HTML. Investigate other ways
-  templateHTML.replace('<!-- REPLACEMEINCODE -->', JSON.stringify(require(`../${inDir}catalog.json`))), 'utf8');
+ncp(`${inDir}`, outDir, function () {
 
 
+  const templateHTML = fs.readFileSync(resDir + 'template.html', 'utf8');
+  fs.writeFileSync(outDir + 'index.html', //Pretty hacky - doing a replace on the HTML. Investigate other ways
+    templateHTML.replace('<!-- REPLACEMEINCODE -->', JSON.stringify(require(`../${inDir}catalog.json`))), 'utf8');
 
-//TODO: keeping the process alive until all streams are closed (it may be that this isn't even needed)
-var checksum;
 
-const checkState = _ => { //This is icky, but we kinda need it
-  setTimeout(_ => {
-    dirsum.digest(outDir, 'sha1', function (err, hashes) {
-      if (!hashes) return console.log('CheckState ran into an issue, limping along anyway.');
-      if (checksum === hashes.hash) {
+  console.log('Creating Zim file...');
 
-        console.log('Creating Zim file...');
+  const exportProc = spawn(`./export2zim`, [`PHET-${Object.keys(config.languageMappings).join('-')}.zim`]);
 
-        const exportProc = spawn(`./export2zim`, [`PHET-${Object.keys(config.languageMappings).join('-')}.zim`]);
+  exportProc.stdout.on('data', function (data) {    // register one or more handlers
+    console.log('stdout: ' + data);
+  });
 
-        exportProc.stdout.on('data', function (data) {    // register one or more handlers
-          console.log('stdout: ' + data);
-        });
+  exportProc.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
 
-        exportProc.stderr.on('data', function (data) {
-          console.log('stderr: ' + data);
-        });
+  exportProc.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
 
-        exportProc.on('exit', function (code) {
-          console.log('child process exited with code ' + code);
+    console.log('View html file at state/export/index.html');
+    console.log('View ZIM file at dist/PHET-*.zim');
+  });
+});
 
-          console.log('View html file at state/export/index.html');
-          console.log('View ZIM file at dist/PHET-*.zim');
-        });
-      } else {
-        console.log(hashes.hash, checksum)
-        checksum = hashes.hash;
-        checkState();
-      }
-    });
-  }, 4000);
-}
-checkState();
