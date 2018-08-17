dbbd6ae027b53d6bf010e6fb112a29cf6c814c9b
Joe Reeve <joseph.reeve@googlemail.com>
2018-08-17 09:22:13 +0100

    Fixed #42

--- steps/setup.js
+++ steps/setup.js
@@ -7,10 +7,10 @@ rimraf('state', function (err) {
     if (err) console.error(err);
     else {
         fs.mkdir('state', function () {
-            fs.mkdir('state/get');
-            fs.mkdir('state/transform');
-            fs.mkdir('state/export');
-            fs.mkdir('state/tmp');
+            fs.mkdirSync('state/get');
+            fs.mkdirSync('state/transform');
+            fs.mkdirSync('state/export');
+            fs.mkdirSync('state/tmp');
         });
     }
 });
