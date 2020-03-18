0317eafe177e6022e93a1b7fb22028f7cb9c839e
midik <midiman@ya.ru>
2020-03-18 14:53:19 +0300

    more refactoring and improvements

--- steps/transform.ts
+++ steps/transform.ts
@@ -1,157 +1,118 @@
-import md5 from 'md5';
 import * as fs from 'fs';
+import * as md5 from 'md5';
 import * as glob from 'glob';
 import * as path from 'path';
+import * as op from 'object-path';
 import * as cheerio from 'cheerio';
+import {minify} from 'html-minifier';
 import * as imagemin from 'imagemin';
 import asyncPool from 'tiny-async-pool';
 import * as progress from 'cli-progress';
 import * as imageminSvgo from 'imagemin-svgo';
-import {minify as htmlminify} from 'html-minifier';
 import * as imageminGifsicle from 'imagemin-gifsicle';
 import * as imageminPngcrush from 'imagemin-pngcrush';
 import * as imageminJpegoptim from 'imagemin-jpegoptim';
 import * as config from '../config.js';
+import {Base64Entity} from './classes';
 
 
 const inDir = 'state/get/';
 const outDir = 'state/transform/';
-const tmpDir = 'state/tmp/';
-
 
 const transform = async () => {
-  console.log('Converting images:');
-  const images: string[] = await Promise.all(glob.sync(`${inDir}/*.{jpg,jpeg,png,svg}`, {}));
-
+  console.log('Converting images...');
+  // const images: string[] = await Promise.all(glob.sync(`${inDir}/*.{jpg,jpeg,png,svg}`, {}));
+  const images = [];
   const bar = new progress.SingleBar({}, progress.Presets.shades_classic);
+
   bar.start(images.length, 0);
   await asyncPool(
     config.workers,
     images,
-    async (file) => {
-      bar.increment();
-      return imagemin([file], outDir, {
-        plugins: [
-          imageminJpegoptim(),
-          imageminPngcrush(),
-          imageminSvgo(),
-          imageminGifsicle()
-        ]
+    async (file) => imagemin([file], outDir, {
+      plugins: [
+        imageminJpegoptim(),
+        imageminPngcrush(),
+        imageminSvgo(),
+        imageminGifsicle()
+      ]
+    })
+      .catch(err => {
+        const failedFile = err.message.match(/Error in file: (.*)/)[1].trim();
+        console.log(err.message);
+        fs.unlink(failedFile, () => console.log('The following file is invalid and was deleted:', failedFile));
       })
-        .catch(err => {
-          const failedFile = err.message.match(/Error in file: (.*)/)[1].trim();
-          console.log(err.message);
-          fs.unlink(failedFile, () => console.log('The following file is invalid and was deleted:', failedFile));
-        });
-    }
+      .finally(() => bar.increment())
   );
-
   bar.stop();
 
-  await asyncPool(
-    config.workers,
-    images,
-    async (file) => fs.promises.copyFile(inDir + file, tmpDir + file)
-  );
+  console.log('Processing documents...');
+  const documents: string[] = await Promise.all(glob.sync(`${inDir}/*.html`, {}));
 
-  console.log('Processing documents:');
-  const documents: string[] = await Promise.all(glob.sync(`${inDir}/*.png`, {}));
+  bar.start(documents.length, 0);
   await asyncPool(
     config.workers,
     documents,
     async (file) => {
-      let data = (await fs.promises.readFile(inDir + file, 'utf8'));
-      data = extractBase64(file, data);
+      let data = (await fs.promises.readFile(file, 'utf8'));
+      const basename = path.basename(file);
+      data = await extractBase64(basename, data);
       data = removeStrings(data);
-      data = extractLanguageElements(file, data);
-      await fs.promises.writeFile(`${outDir}${file}`, data, 'utf8');
+      data = await extractLanguageElements(basename, data);
+      bar.increment();
+      return fs.promises.writeFile(`${outDir}${basename}`, data, 'utf8');
     }
   );
+  bar.stop();
+  console.log('Done.');
 };
 
 
-const extractLanguageElements = (fileName, html) => {
+const extractLanguageElements = async (fileName, html): Promise<string> => {
   const $ = cheerio.load(html);
 
-  const scripts = $('script').toArray().map(script => {
-    // @ts-ignore // todo
-    return (script.children[0] || {}).data || '';
-  }).filter(a => a);
-
-  return scripts.reduce((acc, script, index) => {
-    const newFileName = md5(script);
-
-
-    // todo refactor this
-    try { // File Exists
-      // @ts-ignore
-      stats = fs.statSync(outDir + newFileName);
-    } catch (e) { // File does not exist
-      fs.writeFileSync(`${outDir}${newFileName}.js`, script.trim(), 'utf8');
-    }
-
-    return acc.replace(script, `</script><script src='${newFileName}.js'>`);
-  }, html);
-
-};
-
-const removeStrings = html => {
-  const htmlSplit = html.split('// ### START THIRD PARTY LICENSE ENTRIES ###');
-  html = htmlSplit[0] + htmlSplit[1].split('// ### END THIRD PARTY LICENSE ENTRIES ###')[1];
-
-  html = htmlminify(html, {
-    removeComments: true,
-  });
+  await Promise.all($('script')
+    .toArray()
+    .map(script => op.get(script, 'children.0.data', ''))
+    .filter(x => x)
+    .map(async (script, index) => {
+      const newFileName = md5(script);
+      await fs.promises.writeFile(`${outDir}${newFileName}.js`, script.trim(), 'utf8');
+      return html.replace(script, `</script><script src='${newFileName}.js'>`);
+    })
+  );
   return html;
 };
 
-const extractBase64 = (fileName, html) => {
+const removeStrings = (html): string => {
+  const htmlSplit = html.split('// ### START THIRD PARTY LICENSE ENTRIES ###');
+  html = htmlSplit[0] + htmlSplit[1].split('// ### END THIRD PARTY LICENSE ENTRIES ###')[1];
+  html = minify(html, {removeComments: true});
+  return html;
+};
+
+const extractBase64 = async (fileName, html): Promise<string> => {
   const b64files = html.match(/( src=)?'data:([A-Za-z-+\/]+);base64,[^']*/g);
 
-  return (b64files || []).reduce((html, b64, index) => {
-    const isInSrc = b64.slice(0, 6) === ' src="';
-    b64 = b64.slice(isInSrc ? 6 : 1);
+  await Promise.all((b64files || [])
+    .map(async b64entry => {
+      const isInSrc = b64entry.slice(0, 6) === ' src="';
+      b64entry = b64entry.slice(isInSrc ? 6 : 1);
 
-    const split = b64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      const b64element = new Base64Entity(b64entry);
+      if (b64element.isEmpty()) return html;
 
-    if (!split || split.length !== 3) {
-      // log('No valid b64, continuing anyway.');
-      return html;
-    }
+      const fileExt = path.extname(b64element.mimeType).slice(1);
+      if (['ogg', 'mpeg'].includes(fileExt)) return html;
 
-    const mimeType = split[1];
-    if (!mimeType) {
-      // log('No mimeType, continuing anyway.');
-      return html;
-    }
+      const newName = md5(b64element.data);
+      html.replace(b64entry, `${newName}.${fileExt}`);
 
-    const fileExt = path.extname(mimeType).slice(1);
+      await fs.promises.writeFile(`${outDir}${newName}.${fileExt}`, b64element.data, {encoding: 'base64'});
+      await fs.promises.writeFile(`${outDir}${fileName}`, html, 'utf8');
+    }));
 
-    if (!split[2].length) return html;
-
-    if (fileExt === 'ogg') return html;
-    if (fileExt === 'mpeg') return html;
-    // if (fileExt === 'gif') return html;
-    // if (fileExt === 'jpeg') return html;
-    // if (fileExt === 'png') return html;
-
-    const isImage = mimeType.split('/')[0] === 'image';
-
-    if (!isImage) console.log(mimeType);
-
-    const newName = md5(split[2]);
-
-    html = html.replace(b64, `${newName}.${fileExt}`);
-
-    // todo refactor this
-    try { // File Exists
-      const stats = fs.statSync(`${tmpDir}${newName}.${fileExt}`);
-    } catch (e) { // File does not exist
-      fs.writeFileSync(`${tmpDir}${newName}.${fileExt}`, split[2], {encoding: 'base64'});
-    }
-    fs.writeFileSync(`${outDir}${fileName}`, html, 'utf8');
-    return html;
-  }, html);
+  return html;
 };
 
 (async () => transform())();
