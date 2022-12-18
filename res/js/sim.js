bc937414fbf4abb88fcfcdfde0ce345ee023e805
Pavlo Karatsiuba <pavel.karatsiuba@gmail.com>
2022-12-18 19:09:19 +0100

    issue#26 Removing old logo from "loading" image. Right now you will see only "loading..." word without logo.
    Removing logo from the right bottom corner of the simulations.

---
+++ res/js/sim.js
@@ -0,0 +1,31 @@
+  const emptyImage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=';
+
+  if(window?.phet?.chipper?.mipmaps && Array.isArray(window.phet.chipper.mipmaps['BRAND/logo.png'])){
+    window.phet.chipper.mipmaps['BRAND/logo.png'].map((image)=>{
+      image.src = emptyImage;
+    });
+  }
+
+  const removeOldLogo = (image) => {
+    if($(image).attr('xlink:href').includes('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAREAAABsCAYAAABNX4YlAAA') ||
+      $(image).attr('xlink:href').includes('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIkAAAA2CAYAAADzuLppAAA') ||
+      $(image).attr('xlink:href').includes(emptyImage)) {
+
+      $( image ).parent().parent().remove();
+    }
+  }
+
+  $(() => {
+    $('body').on('DOMNodeInserted', (e) => {
+
+      if (e.target.tagName === 'image') {
+
+        $(e.target).one("load", () => {
+
+          removeOldLogo(e.target);
+        });
+      }
+    });
+    
+    $('image').each((idx, elm) => removeOldLogo(elm))
+  });
